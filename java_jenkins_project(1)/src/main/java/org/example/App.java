d1817d228f564035b24decdc5ae7726f222a0cf9
EstebanCastillo-rese <estebancastillo.alum@insestatut.cat>
2026-04-07 19:57:51 +0200

    Delete java_jenkins_project(1) directory

--- java_jenkins_project(1)/src/main/java/org/example/App.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class App {
-    public static void main(String[] args) {
-        Calculadora calc = new Calculadora();
-        System.out.println("5 + 3 = " + calc.sumar(5, 3));
-    }
-}
